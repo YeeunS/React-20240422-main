fa64d0ac4ce2dd17e38917cef316f694b6b148b6
Yeeun Shin <yeeun2@hawaii.edu>
2024-06-02 21:26:17 -1000

    uncomment

--- React-Training/my-redux-project/src/index.js
+++ React-Training/my-redux-project/src/index.js
@@ -1,34 +1,27 @@
-import createStore from "./store/createStore";
-import todos from "./reducers/todos";
+// import createStore from "./store/createStore";
+// import todos from "./reducers/todos";
 
-// Create the store with the todos reducer and an initial state
-const store = createStore(todos, ["Use Redux"]);
+// const store = createStore(todos, ["Use Redux"]);
 
-// Subscribe to state changes
-const unsubscribe = store.subscribe((action) => {
-  console.log("Action dispatched:", action);
-  console.log("New state:", store.getState());
-});
+// const unsubscribe = store.subscribe((action) => {
+//   console.log("Action dispatched:", action);
+//   console.log("New state:", store.getState());
+// });
 
-// Dispatch an action to add a new todo item
-store.dispatch({
-  type: "ADD_TODO",
-  text: "Read the docs",
-});
+// store.dispatch({
+//   type: "ADD_TODO",
+//   text: "Read the docs",
+// });
 
-// Log the current state
-console.log(store.getState());
-// Output: ['Use Redux', 'Read the docs']
+// console.log(store.getState());
+// // Output: ['Use Redux', 'Read the docs']
 
-// Unsubscribe from state changes
-unsubscribe();
+// unsubscribe();
 
-// Dispatch another action to verify the listener is not called anymore
-store.dispatch({
-  type: "ADD_TODO",
-  text: "Write code",
-});
+// store.dispatch({
+//   type: "ADD_TODO",
+//   text: "Write code",
+// });
 
-// Log the final state
-console.log(store.getState());
-// Output: ['Use Redux', 'Read the docs', 'Write code']
+// console.log(store.getState());
+// // Output: ['Use Redux', 'Read the docs', 'Write code']
