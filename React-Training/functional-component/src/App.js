c489d9be9e920f722e9ff81e28849b52f5c8379b
Yeeun Shin <yeeun2@hawaii.edu>
2024-06-10 16:37:21 -1000

    testing

--- React-Training/functional-component/src/App.js
+++ React-Training/functional-component/src/App.js
@@ -6,16 +6,63 @@ import {
   FunctionalComponentWithCounter,
   FunctionalApp,
 } from "./components/FunctionalApp";
-import Counter from "./components/Counter";
+import Counter, { CounterContainer } from "./components/Counter/Counter";
 import Counter2 from "./components/Counter2";
 import CarApp from "./components/CarApp/CarApp";
 import CarProvider from "./components/CarApp/CarContext";
 import ReduxCarApp from "./components/CarApp/ReduxCarApp";
+import ReduxCounter from "./components/ReduxCounter";
+import TodoList from "./components/RouterComponents/TodoList";
+import Home from "./components/RouterComponents/Home";
+import MyLink from "./components/Router/MyLink";
+import MyRoute from "./components/Router/MyRoute";
+import MyRoutes from "./components/Router/MyRoutes";
+
+/**
+ *
+ * Link - a link/anchor tag for a path
+ * Route - render the element corresponding to the path
+ * Routes - find the route that matches the current path
+ * BrowserRouter - store the current path, provide the routing context
+ *
+ * history - no reload, use relative pathname, no domain/host name
+ * pushState
+ * replaceState
+ * forward
+ * back
+ * go
+ *
+ * location - will cause reload, use full url
+ * assign
+ * replace
+ * reload
+ *
+ * why we need router
+ * spa, navigation won't cause reload
+ * to use url to access a specific page
+ * to have access to the history feature
+ */
 
 function App() {
   return (
     <div className="App">
-      <ReduxCarApp />
+      {/* <nav>
+        <ul>
+          <li>
+            <MyLink to={"/"}>Home</MyLink>
+          </li>
+          <li>
+            <MyLink to={"/todolist"}>TodoList</MyLink>
+          </li>
+        </ul>
+      </nav>
+      <MyRoutes>
+        <MyRoute path="/" element={<Home />} />
+        <MyRoute path="/todolist" element={<TodoList />} />
+      </MyRoutes> */}
+      <CounterContainer />
+      {/* <ReduxCarApp /> */}
+      {/* <ReduxCounter /> */}
       {/* <CarProvider>
         <CarApp />
       </CarProvider> */}
--- React-Training/functional-component/src/components/ReduxCounter.jsx
+++ React-Training/functional-component/src/components/ReduxCounter.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Add, store } from "../redux";
+import { decrement, increment } from "../redux/Slice/CounterSlice.ts";
 
 const ReduxCounter = () => {
   const dispatch = useDispatch();
+  // const count = useSelector((state) => state.count);
+  // console.log(store.getState());
+  const count = useSelector((state) => state.counter.value);
+
   return (
     <div>
-      <button onClick={() => {}}>add</button>
-      <button onClick={() => dispatch(Add)}>add</button>
+      <div>Counter: {count}</div>
+      <button onClick={() => dispatch(decrement())}>minus</button>
+      <button onClick={() => dispatch(increment())}>add</button>
     </div>
   );
 };
